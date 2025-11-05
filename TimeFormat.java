a3bbc82d3cb3801d7483052da197acc38f8dd67c
NoaSz <noa.szafran@post.runi.ac.il>
2025-11-05 16:26:53 +0200

    Completed assignment

--- TimeFormat.java
+++ TimeFormat.java
@@ -12,6 +12,29 @@ public class TimeFormat {
 		int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
 		// Does the same with the minutes part of the input.
 		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
-        // Replace this comment with the rest of your code
+       
+		String suffix = "";
+		if (hours < 12) {
+			suffix = "AM";
+		} else {
+			suffix = "PM";
+		}
+
+		if (hours > 12) {
+			hours = hours - 12;
+		} else if (hours == 24) {
+			hours = 0;
+		}
+
+
+		System.out.println(hours);
+		System.out.println(":")
+		
+		if (minutes < 10) {
+			System.out.println("0");
+		}
+		System.out.println(minutes);
+		System.out.println(" ");
+		System.out.println(suffix);
+		} 
 	}
-}
